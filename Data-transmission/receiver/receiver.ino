738817c14e305fc2ea0ad7688f38251c70783b3d
Souleymane Guerida <33558585+WickedSs@users.noreply.github.com>
2018-09-04 10:15:03 +0200

    Add files via upload

---
+++ Data-transmission/receiver/receiver.ino
@@ -0,0 +1,70 @@
+#include <LiquidCrystal_I2C.h>
+
+int R_clock = 3;
+int R_data = 2;
+
+
+char message[16];
+volatile byte r_bytes = 0;
+volatile int bit_pos = 0;
+volatile bool update_lcd = true;
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
+void setup() {
+  pinMode(R_data, INPUT);
+  strcpy(message, "");
+  lcd.begin(16, 2);
+  attachInterrupt(digitalPinToInterrupt(R_clock), onClock, RISING);
+  
+}
+
+void onClock() {
+  bool r_bite = digitalRead(R_data);
+
+  if(bit_pos == 8) {
+    r_bytes = 0;
+    bit_pos = 0;
+  }
+
+  if (r_bite) {
+    r_bytes |= (0x80 >> bit_pos);
+  }
+
+  bit_pos += 1;
+
+  if (bit_pos == 8) {
+    strncat(message, &r_bytes, 1);
+  }
+
+  update_lcd = true;
+  
+}
+
+
+void loop() {
+  if (update_lcd) {
+    update_lcd = false;
+
+
+    lcd.noCursor();
+    lcd.setCursor(0, 0);
+    lcd.print(message);
+    lcd.setCursor(0, 1);
+    
+    for (int i = 0; i < 8; i += 1) {
+      if (i < bit_pos) {
+        lcd.print(r_bytes & (0x80 >> i) ? "1" : "0");
+      } else {
+        lcd.print(" ");
+      }
+    }
+
+    lcd.setCursor(strlen(message), 0);
+    lcd.cursor();
+  }
+}
+
+
+
+
---
+++ Data-transmission/sender/sender.ino
@@ -0,0 +1,57 @@
+#include <LiquidCrystal_I2C.h>
+
+int T_clock = 3;
+int T_data = 2;
+
+const char *message = "Wicked was Here!";
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
+
+void setup() {
+  // configure the pins to output
+  pinMode(T_clock, OUTPUT);
+  pinMode(T_data, OUTPUT);
+
+  // initialize the lcd 
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0);
+  lcd.print(message);
+
+  for (int bytes = 0; bytes < strlen(message); bytes++ ) {
+    char t_bytes = message[bytes]; 
+    lcd.noCursor();
+    lcd.setCursor(0, 1);
+    lcd.print("        ");
+    lcd.setCursor(bytes, 0);
+    lcd.cursor();
+  
+    for (int bite = 0; bite < 8; bite++) {
+        bool t_bite = t_bytes & (0x80 >> bite);
+  
+        digitalWrite(T_data, t_bite);
+        delay((1000 / 5 ) / 2);    
+  
+        // udpate the lcd
+  
+        lcd.noCursor();
+        lcd.setCursor(bite, 1);
+        lcd.print(t_bite ? "1" : "0");
+        lcd.setCursor(bytes, 0);
+        lcd.cursor();
+  
+  
+        /// Clock
+        digitalWrite(T_clock, HIGH);
+        delay( (1000 / 5 ) / 2);
+        digitalWrite(T_clock, LOW);
+    }
+  }
+
+  digitalWrite(T_data, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
